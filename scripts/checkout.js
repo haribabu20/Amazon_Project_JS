7f5246a36f17cbd51136996a7dced48b2be478bb
haribabu20 <hariselva2000@gmail.com>
2024-06-30 22:24:41 +0530

    Pending code for generating the HTML of Deliveryoption

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -66,45 +66,7 @@ cart.forEach((cartItem) => {
         <div class="delivery-options-title">
           Choose a delivery option:
         </div>
-        <div class="delivery-option">
-          <input type="radio" checked
-            class="delivery-option-input"
-            name="delivery-option-${matchingProduct.id}">
-          <div>
-            <div class="delivery-option-date">
-              Tuesday, June 21
-            </div>
-            <div class="delivery-option-price">
-              FREE Shipping
-            </div>
-          </div>
-        </div>
-        <div class="delivery-option">
-          <input type="radio"
-            class="delivery-option-input"
-            name="delivery-option-${matchingProduct.id}">
-          <div>
-            <div class="delivery-option-date">
-              Wednesday, June 15
-            </div>
-            <div class="delivery-option-price">
-              $4.99 - Shipping
-            </div>
-          </div>
-        </div>
-        <div class="delivery-option">
-          <input type="radio"
-            class="delivery-option-input"
-            name="delivery-option-${matchingProduct.id}">
-          <div>
-            <div class="delivery-option-date">
-              Monday, June 13
-            </div>
-            <div class="delivery-option-price">
-              $9.99 - Shipping
-            </div>
-          </div>
-       </div>
+        ${deliveryOptionsHTML(matchingProduct)}
       </div>
     </div>
 </div>
@@ -112,7 +74,7 @@ cart.forEach((cartItem) => {
 });
 
 
-function deliveryOptionsHTML() {
+function deliveryOptionsHTML(matchingProduct) {
 
   let html = '';
 
