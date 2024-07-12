311896a88988a285406045820d42c537f220ffec
haribabu20 <hariselva2000@gmail.com>
2024-07-12 07:04:36 +0530

    15h - Delete link Update - Instead of DOM and updating delete(), Regenarate the HTML for order summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -129,7 +129,7 @@ export function renderOrderSummary(){
     const container = document.querySelector(`.js-cart-item-container-${productId}`);
     container.remove();
 
-
+    renderOrderSummary();
     renderPaymentSummary();
     
   })
