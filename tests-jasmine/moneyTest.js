df3ff7acc05d5c17a1711ab2acf39035c836ecc0
haribabu20 <hariselva2000@gmail.com>
2024-07-13 14:22:07 +0530

    Further More test cases included for the 'Format Currency' test suite

--- tests-jasmine/moneyTest.js
+++ tests-jasmine/moneyTest.js
@@ -5,4 +5,14 @@ describe('test suite: Format Currency', () => {
   it('Converts cents into dollars', () => {
     expect(formatCurrency(2095)).toEqual('20.95');
   });
+
+  it('Works with 0', () => {
+    expect(formatCurrency(0)).toEqual('0.00');
+  });
+
+  it('rounds up to the nearest cent for 2000.5', () => {
+    expect(formatCurrency(2000.5)).toEqual('20.01')
+  });
 });
+
+
