69d4f102eb184e9c690675513cd9f288f59ceb4e
haribabu20 <hariselva2000@gmail.com>
2024-07-15 20:44:27 +0530

    update on testing delete link button in cart of a product

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -54,7 +54,8 @@ export function renderOrderSummary(){
           data-product-id = "${matchingProduct.id}">
           Save
         </span>
-        <span class="delete-quantity-link link-primary js-delete-link" 
+        <span class="delete-quantity-link link-primary js-delete-link 
+          js-delete-link-${matchingProduct.id}" 
           data-product-id = "${matchingProduct.id}">
           Delete
         </span>
--- tests-jasmine/checkout/orderSummaryTest.js
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -1,5 +1,5 @@
 import { renderOrderSummary } from '../../scripts/checkout/orderSummary.js';
-import { loadFromStorage } from '../../data/cart.js';
+import { loadFromStorage, cart } from '../../data/cart.js';
 
 
 
@@ -45,6 +45,60 @@ describe('test suit: renderOrderSummary', () => {
       document.querySelector(`.js-product-quantity-${productId2}`).innerText)
         .toContain('Quantity: 1');
 
+    document.querySelector('.js-test-container').innerHTML = '';
+  });
+
+
+  it('removes a product - delete', () => {
+
+    spyOn(localStorage, 'setItem');
+
+    document.querySelector('.js-test-container').innerHTML = 
+    `
+      <div class="js-order-summary"></div>
+      <div class="js-payment-summary"></div>
+      <div class="js-checkout-header"></div>
+
+    `;    
+
+
+    
+
+    spyOn(localStorage,'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: productId1,
+        quantity: 2,
+        deliveryOptionId: '1'
+      },
+      {
+        productId: productId2,
+        quantity: 1, 
+        deliveryOptionId: '2'
+      }]);
+    });
+    
+    loadFromStorage();
+
+    renderOrderSummary();
+
+    document.querySelector(`.js-delete-link-${productId1}`).click();
+    
+    expect(
+      document.querySelectorAll('.js-cart-item-container').length)
+      .toEqual(1);
+
+    expect(
+      document.querySelector(`.js-cart-item-container-${productId1}`)
+    ).toEqual(null);
+
+    expect(
+      document.querySelector(`.js-cart-item-container-${productId2}`)
+    ).not.toEqual(null);
+
+    expect(cart.length).toEqual(1);
+    expect(cart[0].productId).toEqual(productId2);
+
+    document.querySelector('.js-test-container').innerHTML = '';
 
   });
 });
