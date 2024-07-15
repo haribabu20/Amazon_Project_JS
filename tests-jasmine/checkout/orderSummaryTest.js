36d100453429524388f668377f0a6085ba2d8fb6
haribabu20 <hariselva2000@gmail.com>
2024-07-15 16:20:47 +0530

    Tests - (1) Number of products added in cart (2) product quantity displayed in page or not

--- tests-jasmine/checkout/orderSummaryTest.js
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -5,6 +5,10 @@ import { loadFromStorage } from '../../data/cart.js';
 
 
 describe('test suit: renderOrderSummary', () => {
+
+  const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
+  const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
   it('displays the cart', () => {
     document.querySelector('.js-test-container').innerHTML = `
       <div class="js-order-summary"></div>
@@ -12,12 +16,12 @@ describe('test suit: renderOrderSummary', () => {
 
     spyOn(localStorage,'getItem').and.callFake(() => {
       return JSON.stringify([{
-        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        productId: productId1,
         quantity: 2,
         deliveryOptionId: '1'
       },
       {
-        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        productId: productId2,
         quantity: 1, 
         deliveryOptionId: '2'
       }]);
@@ -26,6 +30,21 @@ describe('test suit: renderOrderSummary', () => {
     loadFromStorage();
 
     renderOrderSummary();
+
+
+    expect(
+      document.querySelectorAll('.js-cart-item-container').length)
+      .toEqual(2);
+
+    expect(
+      document.querySelector(`.js-product-quantity-${productId1}`).innerText)
+        .toContain('Quantity: 2');
+
+
+    expect(
+      document.querySelector(`.js-product-quantity-${productId2}`).innerText)
+        .toContain('Quantity: 1');
+
 
   });
 });
