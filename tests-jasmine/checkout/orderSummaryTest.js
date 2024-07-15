8a46782da09c721f997b2dd8064ec6937d5c0a1f
haribabu20 <hariselva2000@gmail.com>
2024-07-15 21:02:41 +0530

    Intro to Hooks

--- tests-jasmine/checkout/orderSummaryTest.js
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -9,10 +9,17 @@ describe('test suit: renderOrderSummary', () => {
   const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
 
-  it('displays the cart', () => {
-    document.querySelector('.js-test-container').innerHTML = `
+  beforeEach(() => {
+
+    spyOn(localStorage, 'setItem');
+
+    document.querySelector('.js-test-container').innerHTML = 
+    `
       <div class="js-order-summary"></div>
-    `;
+      <div class="js-payment-summary"></div>
+      <div class="js-checkout-header"></div>
+
+    `;   
 
     spyOn(localStorage,'getItem').and.callFake(() => {
       return JSON.stringify([{
@@ -28,10 +35,13 @@ describe('test suit: renderOrderSummary', () => {
     });
     
     loadFromStorage();
-
     renderOrderSummary();
+  });
 
 
+
+  it('displays the cart', () => {
+   
     expect(
       document.querySelectorAll('.js-cart-item-container').length)
       .toEqual(2);
@@ -50,36 +60,6 @@ describe('test suit: renderOrderSummary', () => {
 
 
   it('removes a product - delete', () => {
-
-    spyOn(localStorage, 'setItem');
-
-    document.querySelector('.js-test-container').innerHTML = 
-    `
-      <div class="js-order-summary"></div>
-      <div class="js-payment-summary"></div>
-      <div class="js-checkout-header"></div>
-
-    `;    
-
-
-    
-
-    spyOn(localStorage,'getItem').and.callFake(() => {
-      return JSON.stringify([{
-        productId: productId1,
-        quantity: 2,
-        deliveryOptionId: '1'
-      },
-      {
-        productId: productId2,
-        quantity: 1, 
-        deliveryOptionId: '2'
-      }]);
-    });
-    
-    loadFromStorage();
-
-    renderOrderSummary();
 
     document.querySelector(`.js-delete-link-${productId1}`).click();
     
