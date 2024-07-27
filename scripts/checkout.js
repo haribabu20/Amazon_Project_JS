d1f58faa6e224a5ab4cb5d306fa7d3235274e244
haribabu20 <hariselva2000@gmail.com>
2024-07-27 17:30:43 +0530

    Made Checkout Page interactive using Backend for products fetching

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,14 @@
+import { loadProducts } from '../data/products.js';
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummay.js';
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 
-renderOrderSummary();
-renderPaymentSummary();
-renderCheckoutHeader();
+loadProducts(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+  renderCheckoutHeader();
+});
+
+
