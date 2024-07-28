8222cd93f9015f822d3ae2a9790866f10ccfd5a9
haribabu20 <hariselva2000@gmail.com>
2024-07-28 12:11:28 +0530

    Adding loadproducts in beforeAll hooks

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from '../../scripts/checkout/orderSummary.js';
 import { loadFromStorage, cart } from '../../data/cart.js';
+import { loadProducts } from '../../data/products.js';
 
 
 
@@ -8,6 +9,14 @@ describe('test suit: renderOrderSummary', () => {
 
   const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+    
+
+  beforeAll((done) => {
+    loadProducts(() => {
+      done();
+    });
+  });
+
 
   beforeEach(() => {
 
@@ -80,5 +89,6 @@ describe('test suit: renderOrderSummary', () => {
 
     document.querySelector('.js-test-container').innerHTML = '';
 
-  });
-});
+    });
+  }); 
+
