73225c0f86fe37b5fb32280fcda6aada9cef157c
haribabu20 <hariselva2000@gmail.com>
2024-08-02 06:58:02 +0530

    POST request to backend and received response from backend

--- scripts/checkout/paymentSummay.js
+++ scripts/checkout/paymentSummay.js
@@ -66,7 +66,8 @@ export function renderPaymentSummary(){
     </div>
   </div>
 
-  <button class="place-order-button button-primary">
+  <button class="place-order-button button-primary
+    js-place-order">
     Place your order
   </button>
   `;
@@ -74,5 +75,21 @@ export function renderPaymentSummary(){
 
   document.querySelector('.js-payment-summary')
     .innerHTML = paymentSummaryHTML;
+
+  document.querySelector('.js-place-order')
+    .addEventListener('click',async () => {
+      const response = await fetch('https://supersimplebackend.dev/orders',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          cart: cart
+        })
+      });
+      const order = await response.json();
+      console.log(order);
+      
+    });
   
 }
